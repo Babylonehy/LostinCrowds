97277bf19c60bfc3e0287c5a21a1a3c54ed5ca51
Sean Lee <u6716878@anu.edu.au>
2019-05-13 16:41:39 +1000

    Reformate Code.
    Signed-off-by: Sean Lee <u6716878@anu.edu.au>

--- app/src/main/java/com/example/lostincrowds/Network/ConstantValue.java
+++ app/src/main/java/com/example/lostincrowds/Network/ConstantValue.java
@@ -3,7 +3,7 @@ package com.example.lostincrowds.Network;
 public final class ConstantValue {
     public final static String successInitial = "-100";
     public final static String successGet = "1";
-    public final static int levelInitial = 1;
-    public final static String MessageInitial = "Initial";
+    final static int levelInitial = 1;
+    final static String MessageInitial = "Initial";
     static final String BASE_URL = "http://52.163.84.2/api/lostincrowds/";
 }
