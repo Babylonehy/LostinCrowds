6daaa6fb0ccb4105129fcd8b964731c306123ca2
Xiaorong Ma <u6641690@anu.edu.au>
2019-05-13 15:07:17 +1000

    basicItem and Line

--- app/src/main/java/com/example/lostincrowds/UI/Item.java
+++ app/src/main/java/com/example/lostincrowds/UI/Item.java
@@ -1,4 +1,41 @@
 package com.example.lostincrowds.UI;
 
 public class Item {
+    public int color;
+    public double percentage;
+    public double locationX;
+    public double locationY;
+
+    public Item(int color, double percentage,double locationX, double locationY){
+        this.color=color;
+        this.percentage=percentage;
+        this.locationX=locationX;
+        this.locationY=locationY;
+
+    }
+
+    public void setLocationX(double x){
+        locationX = x;
+    }
+    public void setLocationY(double x){
+        locationY = x;
+    }
+    public void setPercentage(double x){percentage=x;}
+    public void  setColor(int x){color=x;}
+
+    public double getLocationX() {
+        return locationX;
+    }
+
+    public double getLocationY() {
+        return locationY;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public int getColor() {
+        return color;
+    }
 }
---
+++ app/src/main/java/com/example/lostincrowds/UI/Line.java
@@ -0,0 +1,59 @@
+package com.example.lostincrowds.UI;
+
+import java.util.List;
+
+public class Line {
+    public double startX;
+    public double startY;
+    public double endX;
+    public double endY;
+    public boolean cut;
+    public Line(double startX, double startY, double endX , double endY, boolean cut){
+        this.startX=startX;
+        this.startY=startY;
+
+        this.endX=endX;
+        this.endY=endY;
+        this.cut=cut;
+    }
+
+    public void setCut(boolean cut) {
+        this.cut = cut;
+    }
+
+    public boolean isCut() {
+        return cut;
+    }
+
+    public double getEndX() {
+        return endX;
+    }
+
+    public double getEndY() {
+        return endY;
+    }
+
+    public double getStartX() {
+        return startX;
+    }
+
+    public double getStartY() {
+        return startY;
+    }
+
+    public void setEndX(double endX) {
+        this.endX = endX;
+    }
+
+    public void setEndY(double endY) {
+        this.endY = endY;
+    }
+
+    public void setStartX(double startX) {
+        this.startX = startX;
+    }
+
+    public void setStartY(double startY) {
+        this.startY = startY;
+    }
+}
