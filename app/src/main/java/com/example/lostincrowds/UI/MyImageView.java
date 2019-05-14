1152abacdd807ef376dd6c5014746f53a63fd156
Sean Lee <u6716878@anu.edu.au>
2019-05-15 00:26:01 +1000

    Myview Done.
    Signed-off-by: Sean Lee <u6716878@anu.edu.au>

--- app/src/main/java/com/example/lostincrowds/UI/MyImageView.java
+++ app/src/main/java/com/example/lostincrowds/UI/MyImageView.java
@@ -1,16 +1,19 @@
 package com.example.lostincrowds.UI;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.support.annotation.DrawableRes;
+import android.support.annotation.IdRes;
 import android.support.constraint.ConstraintLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 
-import com.daimajia.androidanimations.library.Techniques;
-import com.daimajia.androidanimations.library.YoYo;
 import com.daimajia.numberprogressbar.NumberProgressBar;
 import com.example.lostincrowds.R;
 import com.github.florent37.viewanimator.ViewAnimator;
+import com.github.lzyzsd.circleprogress.DonutProgress;
 
 import static com.example.lostincrowds.Network.ConstantValue.HEIGHT;
 import static com.example.lostincrowds.Network.ConstantValue.WIDTH;
@@ -18,32 +21,47 @@ import static com.example.lostincrowds.Network.ConstantValue.WIDTH;
 public class MyImageView extends ConstraintLayout {
 
     private BasicImageView image;
-    private NumberProgressBar number_progress_bar;
+    private DonutProgress donutProgress;
     private float xpos;
     private float ypos;
 
-    private float percentage;
-    public MyImageView ( Context context , @DrawableRes int back , @DrawableRes int front , float xpos , float ypos , float percentage ) {
-        super(context);
 
+    private String id;
+    private float percentage;
+
+    @SuppressLint("ResourceType")
+    public MyImageView ( Context context , @DrawableRes int back , @DrawableRes int front , float xpos , float ypos , float percentage , String id ) {
+        super(context);
+        donutProgress = new DonutProgress(context);
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.myimageview , this);
         Log.v("MyImageView" , "Constructe");
 
         this.xpos = xpos;
         this.ypos = ypos;
+        this.id = id;
         setX(xpos);
         setY(ypos);
         this.percentage = percentage;
 
         image = findViewById(R.id.basicImageView);
+        image.setId(View.NO_ID);
         image.setBasicImageView(back , front);
         image.setAdjustViewBounds(true);
-        number_progress_bar = findViewById(R.id.number_progress_bar);
-        number_progress_bar.setProgress((int) percentage);
-        number_progress_bar.setMax(100);
+//        number_progress_bar = findViewById(R.id.number_progress_bar);
+//        number_progress_bar.setProgress((int) percentage);
+//        number_progress_bar.setMax(100);
+        donutProgress = findViewById(R.id.donut_progress);
+        donutProgress.setId(View.NO_ID);
+//        donutProgress.setDonut_progress(String.valueOf(percentage));
+        donutProgress.setFinishedStrokeWidth(10);
+        donutProgress.setTextSize(20);
+        donutProgress.setMax(100);
+        donutProgress.setProgress((int) percentage);
+        donutProgress.setUnfinishedStrokeWidth(5);
         Log.v("Myview" , getXpos() + " " + getYpos());
         setMyAnimation();
+
     }
 
 
@@ -65,6 +83,15 @@ public class MyImageView extends ConstraintLayout {
 
     public float getPercentage () {
         return percentage;
+    }
+
+    public void setPercentage ( float percentage ) {
+        this.percentage = percentage;
+        donutProgress.setProgress(percentage);
+    }
+
+    public String getViewId () {
+        return id;
     }
 //        image=new BasicImageView(context,back,front);
 //        number_progress_bar=new NumberProgressBar(context);
--- app/src/main/java/com/example/lostincrowds/UI/UI_test.java
+++ app/src/main/java/com/example/lostincrowds/UI/UI_test.java
@@ -5,16 +5,12 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.LinearLayout;
 
-import com.daimajia.androidanimations.library.Techniques;
 import com.example.lostincrowds.R;
-import com.github.florent37.viewanimator.ViewAnimator;
-
-import at.wirecube.additiveanimations.additive_animator.AdditiveAnimator;
 
 import static com.example.lostincrowds.Network.ConstantValue.HEIGHT;
 import static com.example.lostincrowds.Network.ConstantValue.WIDTH;
@@ -41,11 +37,15 @@ public class UI_test extends AppCompatActivity {
         basicImageView.setBasicImageView(R.drawable.gray , R.drawable.eyesclose);
         addContentView(basicImageView , basicImageView.getLayoutParams());
 
-        MyImageView testview = new MyImageView(this , R.drawable.gray , R.drawable.eyesclose , 100 , 600 , 50);
+        MyImageView testview = new MyImageView(this , R.drawable.gray , R.drawable.eyesclose , 100 , 600 , 50 , "S1");
+        MyImageView testview2 = new MyImageView(this , R.drawable.gray , R.drawable.eyesclose , 300 , 600 , 100 , "S2");
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(HEIGHT ,
                 WIDTH);
         addContentView(testview , params);
-
+        testview.setPercentage(40);
+        addContentView(testview2 , new LinearLayout.LayoutParams(HEIGHT , WIDTH));
+        testview2.setPercentage(60);
+        Log.v("Myview" , testview.equals(testview2) ? "T" : "F");
     }
 
 }
