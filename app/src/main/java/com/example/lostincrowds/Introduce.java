19c0542b723e095b6c4655342ba9b2fc1c85f03b
Sean Lee <u6716878@anu.edu.au>
2019-05-24 16:17:59 +1000

    Update introduction.
    Signed-off-by: Sean Lee <u6716878@anu.edu.au>

--- app/src/main/java/com/example/lostincrowds/Introduce.java
+++ app/src/main/java/com/example/lostincrowds/Introduce.java
@@ -6,6 +6,12 @@ package com.example.lostincrowds;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import me.grantland.widget.AutofitTextView;
 
 public class Introduce extends AppCompatActivity {
 
@@ -13,6 +19,10 @@ public class Introduce extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduce);
-
+        AutofitTextView mFileContentView = (AutofitTextView) findViewById(R.id.autofitTextView2);
+        mFileContentView.setMovementMethod(ScrollingMovementMethod.getInstance());
+        AutofitTextView mFileContentView2 = (AutofitTextView) findViewById(R.id.autofitTextView);
+        mFileContentView2.setMovementMethod(ScrollingMovementMethod.getInstance());
+        Glide.with(this).load(R.drawable.cutdraw).into((ImageView) findViewById(R.id.drawlinegif));
     }
 }
